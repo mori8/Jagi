a68e07e603c12cef50540df26de015064619015a
cherrrity <47818004+cherrrity@users.noreply.github.com>
2020-02-21 00:15:57 +0900

    views.py 들여쓰기 오류 수정

--- jagi/app_jagi/views.py
+++ jagi/app_jagi/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def jagi_list(request):
-return render(request, 'blog/jagi_list.html', {})
+    return render(request, 'blog/jagi_list.html', {})
